8ad4c3d0ce2727e81c9fc3da2e85a4ecd1a55b01
Grant Ours <grant.ours55@gmail.com>
2022-07-22 16:08:10 -0600

    commit tree 50eeb0381c365e1407f4d22cedd65aef39c09e44

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+function findMatching(arrayofDrivers, str) {
+  return arrayofDrivers.filter(
+    (name) => name.toUpperCase() === str.toUpperCase()
+  );
+}
+
+function fuzzyMatch(arrayofDrivers, str) {
+  return arrayofDrivers.filter((name) => {
+    return name.toLowerCase().indexOf(str.toLowerCase()) == 0;
+  });
+}
+
+function altFuzzyMatch(driverNames, substring) {
+  return driverNames.filter((name) => {
+    for (let i = 0; i < substring.length; i++) {
+      if (substring[i] != name[i]) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+
+function matchName(arrayofDrivers, str) {
+  return arrayofDrivers.filter((obj) => obj.name === str);
+}
